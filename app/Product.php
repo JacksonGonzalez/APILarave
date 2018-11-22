1109a52220604b82d670f0f2fb0ee294e749db97
JacksonGonzalez <jaesgoca@gmail.com>
2018-11-22 16:29:42 -0500

    se agregan los fillables de los modelos Category y Product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+        'name', 'description', 'quantity', 'status', 'image', 'seller_id'
+    ];
 }
