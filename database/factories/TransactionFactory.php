1d70daf4ade0cf6e85497f219f1a7c92a9a77c59
JacksonGonzalez <jaesgoca@gmail.com>
2018-11-23 14:31:28 -0500

    se agrega el factory de Transaction

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Transaction;
+use App\Seller;
+use App\User;
+
+$factory->define(Transaction::class, function (Faker $faker) {
+
+    $vendedor = Seller::has('products')->get()->random(23);
+    $comprador = User::all()->except($vendedor->id)->random();
+    
+    return [
+        'quantity' => $faker->numberBetween(1, 5),
+        'buyer_id' => $comprador->id,
+        'product_id' => $vendedor->products->random()->id,
+    ];
+});
