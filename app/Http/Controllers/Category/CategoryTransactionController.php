6b12490a0d7ca9b43106124afb14469c4c75db4e
JacksonGonzalez <jaesgoca@gmail.com>
2018-11-29 17:55:38 -0500

    se crea la funcion del index para el CategoryTransactionController

---
+++ app/Http/Controllers/Category/CategoryTransactionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
+
+class CategoryTransactionController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Category $category)
+    {
+        $transactions = $category->products()->whereHas('transactions')->with('transactions')->get()
+        ->pluck('transactions')->collapse();
+
+        dd($transactions);
+
+        // return $this->showAll($transactions);
+    }
+}
