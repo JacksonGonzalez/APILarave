7f17dc669e683e8b73acf0ea5bcad9a33c93fd39
JacksonGonzalez <jaesgoca@gmail.com>
2018-12-01 18:22:57 -0500

    se implementa las funciones de SellerProductController

---
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
+use App\User;
+use App\Product;
+
+class SellerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $products = $seller->products;
+
+        return $this->showAll($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $seller)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+
+        $data['status'] = Product::PRODUCTO_NO_DISPONIBLE;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+
+        return $this->showOne($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller, Product $product)
+    {
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in: '.Product::PRODUCTO_DISPONIBLE.', '.Product::PRODUCTO_NO_DISPONIBLE,
+            'image' => 'image'
+        ];
+
+        $this->validate($request, $rules);
+
+        if($seller->id != $product->seller_id){
+            return $this->errorResponse('El vendedor especificado no es el vendedor real del producto', 422);
+        }
+
+        // $product->fill($request->intersect([
+        //     'name',
+        //     'description',
+        //     'quantity'
+        // ]));
+        if($request->has('name')){
+            $product->name = $request->name ;
+        }
+
+        if($request->has('description')){
+            $product->description = $request->description ;
+        }
+
+        if($request->has('quantity')){
+            $product->quantity = $request->quantity ;
+        }
+        
+        if($request->has('status')){
+            $product->status = $request->status;
+
+            if($product->estaDisponible() && $product->categories()->count() == 0){
+                return $this->errorResponse('Un Producto activo debe tener al menos una categoria', 409);
+            }
+        }
+
+        if($product->isClean()){
+            return $this->errorResponse('Debe especificar por lo menos un valor diferente para actualizar', 422);
+        }
+
+        $product->save();
+
+        return $this->showOne($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller, Product $product)
+    {
+        if($seller->id != $product->seller_id){
+            return $this->errorResponse('El vendedor especificado no es el vendedor real del producto', 422);
+        }
+
+        $product->delete();
+
+        return $this->showOne($product);
+    }
+}
